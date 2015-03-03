5f41d84956425c6b83b8f6f3704eb484c4cd2e11
Alejandro Reyes Bautista <reyes.bautista.alejandro@gmail.com>
2015-03-03 20:57:15 +0100

    modificaciones alumno 2

--- GitAux.java
+++ GitAux.java
@@ -17,9 +17,13 @@ public class GitAux {
     
     public void metodoAlumno2(){
         System.out.println("metodo 2");
-    }
+    }   System.out.println("modificado por alumno2"); // alumno 2
     
     public void metodoComunitario(){
+        // Comentario incluido para pruebas
+       System.out.println("Aqui escribimos todos");
+        // modificaciones alumno 2
+       System.out.println("Alumno 2 inserta esta sentencia");
     }
     	//fin program
     public void testGitAux(){

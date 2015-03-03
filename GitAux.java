fc4a5f14e6eae4e925de9353c3ee9079e6250c47
kheev <goldielpro@uma.es>
2015-03-03 20:52:12 +0100

    gitaux modificado

--- GitAux.java
+++ GitAux.java
@@ -13,6 +13,7 @@ public class GitAux {
     
     public void metodoAlumno1(){
         System.out.println("metodo 1");
+	System.out.println("modificado por alumno 1"); // alumno 1
     }
     
     public void metodoAlumno2(){
@@ -20,6 +21,10 @@ public class GitAux {
     }
     
     public void metodoComunitario(){
+    	// Comentario incluido para pruebas
+	System.out.println("Aquí escribimos todos");
+	// modificaciones alumno 1
+	System.out.println("Alumno 1 inserta esta sentencia");
     }
     	//fin program
     public void testGitAux(){
